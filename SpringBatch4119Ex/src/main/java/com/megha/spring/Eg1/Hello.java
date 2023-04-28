ed757a39e06f1a3200af7d06680d16cc36ea2952
Megha Tomar <122961155+codingwithmegha@users.noreply.github.com>
2023-04-28 16:53:50 +0530

    Update Hello.java

--- SpringBatch4119Ex/src/main/java/com/megha/spring/Eg1/Hello.java
+++ SpringBatch4119Ex/src/main/java/com/megha/spring/Eg1/Hello.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Hello {
 public static void main(String[] args) {
 	ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("application.Context.xml");
-    HelloService obj=(HelloService)context.getBean("Hello");
+    HelloService obj=(HelloService)context.getBean("HelloService");
     obj.hello();
     context.close();
 
