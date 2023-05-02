cfc51aa3c060c751576dd919b789e70368f75ede
Megha Tomar <122961155+codingwithmegha@users.noreply.github.com>
2023-05-02 21:35:41 +0530

    Add files via upload

---
+++ com.megha.springeg4/src/main/java/com/megha/springeg4/MainClass.java
@@ -0,0 +1,14 @@
+package com.megha.springeg4;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainClass {
+public static void main(String... args) {
+ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+Employee e = (Employee) context.getBean("empl");
+System.out.println("Name : "+e.getName());
+System.out.println("Id : "+e.getId());
+}
+}
+
