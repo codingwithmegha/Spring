2ea6ed4774082abad4b5b196053c33fd050bf305
Megha Tomar <122961155+codingwithmegha@users.noreply.github.com>
2023-05-02 21:44:32 +0530

    Add files via upload

---
+++ com.megha.springeg5/src/main/java/com/megha/springeg5/AppConfig.java
@@ -0,0 +1,11 @@
+package com.megha.springeg5;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.megha.springeg5")
+public class AppConfig {
+}
+
+
---
+++ com.megha.springeg5/src/main/java/com/megha/springeg5/MainClass.java
@@ -0,0 +1,14 @@
+package com.megha.springeg5;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainClass {
+public static void main(String... args) {
+AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+Employee e1 = context.getBean(Employee.class);
+e1.knowInfor();
+context.close();
+}
+}
+
+
+
